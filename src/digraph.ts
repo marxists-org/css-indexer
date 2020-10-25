ce820ed37348fedce593c3b6b7be54a06995a606
Philip Mooney <philipm@marxists.org>
2020-10-24 21:45:49 -0700

    wip: filesystem breakout seems to work

--- src/digraph.ts
+++ src/digraph.ts
@@ -1,152 +1,186 @@
-export default class DirectedGraph {
-  #adjacency: Map<string, Set<string>> = new Map();
+import { v4 as uuidv4 } from 'uuid';
 
-  static fromObject(dictionary: {[key:string]: string[]}): DirectedGraph {
-    const graph = new DirectedGraph();
-    Object.entries(dictionary).forEach(entry => {
-      const vertex: string = entry[0];
-      const neighbors: string[] = entry[1];
-      graph.addVertex(vertex);
-      neighbors.forEach(neighbor => graph.addEdge(vertex, neighbor));
-    });
-    return graph;
+export default class DirectedGraph<T> {
+  #adjacency: Map<Symbol, Set<Symbol>> = new Map();
+  #data: Map<Symbol, T> = new Map();
+
+  get size(): number {
+    return this.#adjacency.size;
   }
 
-  static fromArray(list: {vertex: string, edges: string[]}[]): DirectedGraph {
-    const graph = new DirectedGraph();
-    list.forEach(node => {
-      graph.addVertex(node.vertex);
-      node.edges.forEach(neighbor => graph.addEdge(node.vertex, neighbor));
-    });
-    return graph;
+  setVertex(keyOrValue: Symbol|T, valueOrNull: T|null = null): Symbol {
+    if (!((typeof keyOrValue === "symbol" && valueOrNull !== null)
+        || (typeof keyOrValue !== "symbol" && valueOrNull === null))) {
+      throw new Error();
+    }
+    const { key, value } = (typeof keyOrValue === "symbol" && valueOrNull !== null)
+      ? { key: keyOrValue as Symbol, value: valueOrNull as T }
+      : { key: Symbol(), value: keyOrValue as T};
+    if (!this.#adjacency.has(key)) {
+      this.#adjacency.set(key, new Set());
+    }
+    this.#data.set(key, value);
+    return key;
   }
 
-  get vertices(): IterableIterator<string> {
-    return this.#adjacency.keys();
+  addVertex(value: T): Symbol {
+    const key = Symbol();
+    if (!this.#adjacency.has(key)) {
+      this.#adjacency.set(key, new Set());
+    }
+    this.#data.set(key, value);
+    return key;
   }
 
-  getEdges(vertex: string): Array<string> {
-    let setOrUndefined = this.#adjacency.get(vertex);
-    return setOrUndefined == null
-      ? []
-      : [...setOrUndefined.values()];
+  getVertex(key: Symbol): T|undefined {
+    return this.#data.get(key);
   }
 
-  addVertex(name: string) {
-    if (this.#adjacency.has(name)) return;
-    this.#adjacency.set(name, new Set());
-  }
-
-  addEdge(from: string, to: string) {
-    this.addVertex(from);
-    this.addVertex(to);
+  addEdge(from: Symbol, to: Symbol) {
+    if (!this.#adjacency.has(from) || !this.#adjacency.has(from)) throw new Error();
     const adjacency = this.#adjacency.get(from);
     if (adjacency == null) throw new Error('inconsistency');
     if (adjacency.has(to)) return;
     adjacency.add(to);
   }
 
-  reverse(): DirectedGraph {
+  has(key: Symbol): boolean {
+    return this.#adjacency.has(key);
+  }
+
+  toArray(): {vertex: {key: Symbol, value:T}, edges: Symbol[]}[] {
+    return Array.from(this.#adjacency.entries())
+      .map(([vertex, edges]) => {
+        return {
+          vertex: {
+            key: vertex,
+            value: this.#data.get(vertex) as T,
+          },
+          edges: Array.from(edges.values())
+        };
+      });
+  }
+
+  reverse(): DirectedGraph<T> {
     return Array.from(this.#adjacency.entries()).reduce((graph, [from, adjacency]) => {
+      if (!graph.has(from)) {
+        graph.setVertex(from, (this.getVertex(from) as T));
+      }
       adjacency.forEach(to => {
+        if (!graph.has(to)) {
+          graph.setVertex(to, (this.getVertex(to) as T));
+        }
         graph.addEdge(to, from);
       });
       return graph;
-    }, new DirectedGraph());
+    }, new DirectedGraph<T>());
   }
 
-  // walk(vertex: string, visiter: (vertex: string) => void) {
-  //   this.dfs(vertex, new Set(), visiter);
-  // }
-
-  // forEach(visiter: (vertex: string) => void) {
-  //   const vertices = Array.from(this.#adjacency.keys());
-  //   const visited: Set<string> = new Set();
-  //   vertices.forEach(vertex => this.dfs(vertex, visited, visiter));
-  // }
-
-  private dfs(vertex: string, visited: Set<string>, visitor: (vertex: string) => void) {
-    if (visited.has(vertex)) return
-    visited.add(vertex);
-    visitor(vertex);
-    const neighbors = this.#adjacency.get(vertex);
-    if (neighbors == null) throw new Error();
-    Array.from(neighbors.values()).forEach(neighbor => this.dfs(neighbor, visited, visitor));
+  vertices(): IterableIterator<[Symbol, T]> {
+    return this.#data.entries();
   }
 
-
-  topological(vertex: string, cb:(vertex: string)=>void) {
-    this.reverse().postorder(vertex, cb);
+  edges(): IterableIterator<[Symbol, Set<Symbol>]> {
+    return this.#adjacency.entries();
   }
 
-  postorder(vertex: string, cb:(vertex: string)=>void) {
-    const visited: Set<string> = new Set();
-    this.postorder_(vertex, visited, cb);
+  getEdges(key: Symbol): IterableIterator<Symbol> {
+    return this.#adjacency.has(key)
+      ? this.#adjacency.get(key)!.values()
+      : [][Symbol.iterator]()
   }
 
-  private postorder_(vertex: string, visited: Set<string>, cb:(vertex:string)=>void) {
+  postorder(vertex: Symbol): IterableIterator<Symbol> {
+    const visited: Set<Symbol> = new Set();
+    const collected: Array<Symbol> = []
+    this.postorder_(vertex, visited, collected);
+    return collected[Symbol.iterator]();
+  }
+
+  private postorder_(vertex: Symbol, visited: Set<Symbol>, collected: Array<Symbol>) {
     visited.add(vertex);
     const neighbors = this.#adjacency.get(vertex);
     if (neighbors == null) throw new Error();
-    Array.from(neighbors.values()).forEach(neighbor => {
+
+    for (let neighbor of neighbors.values()) {
       if (!visited.has(neighbor)) {
-        this.postorder_(neighbor, visited, cb);
+        this.postorder_(neighbor, visited, collected);
       }
-    });
-    cb(vertex);
+    }
+
+    collected.push(vertex);
   }
 
-  toObject(): {[key:string]: string[]} {
-    return Array.from(this.#adjacency.entries()).reduce((dictionary, [vertex, neighbors]) => {
-      dictionary[vertex] = Array.from(neighbors.values());
-      return dictionary; 
-    }, {} as {[key:string]: string[]});
+  forEach(from: Symbol, visitor: (node: T, key: Symbol)=>void) {
+    const visited = new Set();
+    const adjacency = this.#adjacency;
+    const data = this.#data;
+
+    function traverse(next: Symbol) {
+      if (!visited.has(next)) {
+        const datum = data.get(next);
+        if (datum == null) throw new Error();
+        visitor(datum, next);
+        visited.add(next);
+      }
+      Array.from(adjacency.get(next) || []).forEach(to => traverse(to));
+    }
+
+    traverse(from);
   }
 
-  toArray(): {vertex: string, edges: string[]}[] {
-    return Array.from(this.#adjacency.entries())
-      .map(([vertex, edges]) => ({vertex, edges: Array.from(edges.values())}));
+  transform<U>(
+      mapper: (
+        graph: DirectedGraph<U>,
+        node: T,
+        key: Symbol,
+        adjacency: Symbol[],
+        g: this)=>void): DirectedGraph<U> {
+    return Array.from(this.#adjacency.entries()).reduce((graph, [from, adjacency]) => {
+      mapper(graph, this.getVertex(from) as T, from, Array.from(adjacency.values()), this);
+      return graph;
+    }, new DirectedGraph<U>());
   }
+
+  map<U>(mapper: (node: T, key: Symbol, graph: this) => U ): DirectedGraph<U> {
+    return Array.from(this.#adjacency.entries()).reduce((graph, [from, adjacency]) => {
+      if (!graph.has(from)) {
+        graph.setVertex(from, mapper(this.getVertex(from) as T, from, this));
+      }
+      adjacency.forEach(to => {
+        if (!graph.has(to)) {
+          graph.setVertex(to, mapper(this.getVertex(to) as T, to, this));
+        }
+        graph.addEdge(from, to);
+      });
+      return graph;
+    }, new DirectedGraph<U>());
+  }
+
+  toObject(): {[key:string] : {data: T, edges: string[]}} {
+    const keyToId = new Map<Symbol, string>();
+    return Array.from(this.#adjacency.entries()).reduce((dict, [key, edgeKeys]) => {
+      const data = this.#data.get(key);
+      if (data == null) throw new Error();
+
+      if (!keyToId.has(key)) keyToId.set(key, uuidv4());
+      const id = keyToId.get(key) as string;
+      const edges = Array.from(edgeKeys).map(key => {
+        if (!keyToId.has(key)) keyToId.set(key, uuidv4());
+        return keyToId.get(key) as string;
+      });
+
+      dict[id] = {data, edges};
+      return dict;
+    }, {} as {[key:string] : {data: T, edges: string[]}});
+  }
+
+  toMap(): Map<Symbol, [T, Symbol[]]> {
+    return Array.from(this.#adjacency.entries()).reduce((map, [vertex, edges]) => {
+      const vertexData = this.#data.get(vertex);
+      map.set(vertex, [vertexData, ...edges]);
+      return map;
+    }, new Map());
+  }
+
 }
-
-// const g = new DirectedGraph();
-// g.addEdge("file.html", "works-red.css");
-// g.addEdge("works-red.css", "works.css");
-// g.addEdge("print.css", "works.css");
-// g.addEdge("file.html", "print.css");
-// g.addEdge("file2.html", "works.css");
-// g.addEdge("file3.html", "print.css");
-
-//             file.html
-//             /       \
-//    works-red.css    print.css <-- file3.html
-//            \         /
-//             works.css  <-- file2.html
-// console.log('---dfs (file.html)---');
-// console.log(Array.from(g.topological('file.html')));
-// console.log(Array.from(g.reverse().topological('works.css')));
-
-// const example = new DirectedGraph();
-// example.addVertex('a')
-// example.addVertex('b')
-// example.addVertex('c')
-// example.addVertex('e')
-// example.addVertex('t')
-// example.addEdge('a', 't');
-// example.addEdge('a', 'b');
-// example.addEdge('a', 'c');
-// example.addEdge('t', 'b');
-// example.addEdge('c', 'b');
-// example.addEdge('c', 'e');
-// example.addEdge('e', 'd');
-// example.addEdge('b', 'e');
-
-// console.log('dfs');
-// example.forEach(console.log);
-// console.log('postoder');
-// console.log(Array.from(example.postorder('a')));
-//
-// console.log('reversed graph postoder');
-// console.log(Array.from(example.reverse().postorder('e')));
-
-
